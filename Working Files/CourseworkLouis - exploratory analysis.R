df1ec47f0b53dd3c37b4c92bb81c8d4c3f624447
Louis Bennett <louis21bennett@gmail.com>
2024-03-04 20:57:04 +0000

    Add adjustments for weights, start modelling for Q2

--- Working Files/CourseworkLouis - exploratory analysis.R
+++ Working Files/CourseworkLouis - exploratory analysis.R
@@ -6,14 +6,15 @@ load("Datasets/hsesub.Rdata")
 # Assume drinkYN_19 is binomally distributed with unknown probability p and calculate confidence intervals this way
 # Can do similar for cigarette/e-cig consumption using cigsta3_19 and NDPNow_19 variables
 
-calculate_binomial_ci <- function(dat_vec, alpha = 0.05) {
+calculate_binomial_ci <- function(dat_vec, weights = rep(1, length.out = length(dat_vec)), alpha = 0.05) {
   dat_vec <- as.numeric(dat_vec)
   
   n <- length(dat_vec)
   
-  p_hat <- mean(dat_vec)
+  # Need to check if you can weight like this!
+  p_hat <- sum(dat_vec * weights) / sum(weights)
   
-  var_est <- p_hat * (1 - p_hat) / n
+  var_est <- p_hat * (1 - p_hat) / sum(weights)
   
   ci <- p_hat + c(-1, 1) * qnorm(1 - alpha / 2) * sqrt(var_est)
   
@@ -21,17 +22,20 @@ calculate_binomial_ci <- function(dat_vec, alpha = 0.05) {
 }
 
 # drinking
-overall_drinking <- calculate_binomial_ci(na.omit(subdat$drinkYN_19) == 2)
+drinking_na <- is.na(subdat$drinkYN_19)
+overall_drinking <- calculate_binomial_ci(subdat$drinkYN_19[!drinking_na] == 2, subdat$wt_int[!drinking_na])
 
 100 * overall_drinking$ci
 
 # smoking
-overall_smoking <- calculate_binomial_ci(na.omit(subdat$cigsta3_19) == 1)
+smoking_na <- is.na(subdat$cigsta3_19)
+overall_smoking <- calculate_binomial_ci(subdat$cigsta3_19[!smoking_na] == 1, subdat$wt_int[!smoking_na])
 
 100 * overall_smoking$ci
 
 # e-cigs
-overall_ecigs <- calculate_binomial_ci(na.omit(subdat$NDPNow_19) %in% c(1, 3))
+ecig_na <- is.na(subdat$NDPNow_19)
+overall_ecigs <- calculate_binomial_ci(subdat$NDPNow_19[!ecig_na] %in% c(1, 3), subdat$wt_int[!ecig_na])
 
 100 * overall_ecigs$ci
 
@@ -40,11 +44,11 @@ overall_ecigs <- calculate_binomial_ci(na.omit(subdat$NDPNow_19) %in% c(1, 3))
 age_groups <- attr(subdat$ag16g10, 'labels')
 
 age_groups_drinking <- lapply(age_groups, \(x) {
-  dat_vec <- subdat %>% dplyr::filter(ag16g10 == x) %>% pull(drinkYN_19)
+  dat <- subdat %>% dplyr::filter(ag16g10 == x, !is.na(drinkYN_19)) %>% select(wt_int, drinkYN_19)
   
-  dat_vec <- na.omit(dat_vec) == 2
+  dat_vec <- dat$drinkYN_19 == 2
   
-  summary <- calculate_binomial_ci(dat_vec)
+  summary <- calculate_binomial_ci(dat_vec, dat$wt_int)
   
   tibble::tibble(p_hat = summary$p_hat, lower = summary$ci[1], upper = summary$ci[2])
 }) %>% 
@@ -62,11 +66,11 @@ ggplot(data = age_groups_drinking, aes(x = age_group, y = p_hat, ymax = upper, y
 # Current smokers
 
 age_groups_smoking <- lapply(age_groups, \(x) {
-  dat_vec <- subdat %>% dplyr::filter(ag16g10 == x) %>% pull(cigsta3_19)
+  dat <- subdat %>% dplyr::filter(ag16g10 == x, !is.na(cigsta3_19)) %>% select(wt_int, cigsta3_19)
   
-  dat_vec <- na.omit(dat_vec) == 1
+  dat_vec <- dat$cigsta3_19 == 1
   
-  summary <- calculate_binomial_ci(dat_vec)
+  summary <- calculate_binomial_ci(dat_vec, dat$wt_int)
   
   tibble::tibble(p_hat = summary$p_hat, lower = summary$ci[1], upper = summary$ci[2])
 }) %>% 
@@ -81,3 +85,37 @@ ggplot(data = age_groups_smoking, aes(x = age_group, y = p_hat, ymax = upper, ym
   ylab('% Current Smokers') +
   theme_minimal()
 
+
+# Assume a poisson distribution for the number of cigarettes smoked a week
+dat_vec <- na.omit(subdat$cigdyal_19[subdat$cigdyal_19 != 0]) * 7
+sum(dat_vec) / length(dat_vec)
+
+## Question two model
+
+# Cleaning data
+
+relevant_factors <- c('Sex', 'topqual2', 'marstatD', 'urban14b', 'origin2', 'GOR1', 'Age35g', 'qimd19')
+
+subdat_clean <- subdat %>% 
+  filter(!ag16g10 %in% c(1:3), !is.na(cigdyal_19)) %>% 
+  mutate(
+    across(all_of(relevant_factors), as.factor), cigweek_19 = 7 * cigdyal_19
+  ) %>% 
+  select(
+    cigweek_19, all_of(relevant_factors)
+  )
+
+full_model <- glm(formula = cigweek_19 ~ Age35g:qimd19 + Age35g:Sex + ., family = poisson, data = subdat_clean)
+null_model <- glm(formula = cigweek_19 ~ 1, family = poisson, data = subdat_clean)
+
+fitted_model <- step(full_model, scope = list(lower = null_model, upper = full_model), direction = 'backward', trace = 0)
+
+summary(fitted_model)
+
+formula <- cigweek_19 ~ Sex + topqual2 + marstatD + qimd19 + urban14b + origin2 + GOR1 + Age35g
+
+mod <- glm(formula = formula, family = gaussian, data = subdat_clean)
+
+summary(mod)
+
+plot(mod, 2)
